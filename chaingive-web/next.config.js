9bf1ffade6dfaffbb12c483f5ac4bf1d31806a31
mtfury21 <mtfury21@gmail.com>
2025-10-24 00:00:44 +0000

    edits-

--- chaingive-web/next.config.js
+++ chaingive-web/next.config.js
@@ -1,19 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
   },
   images: {
     domains: ['api.chaingive.ng', 'localhost'],
+    unoptimized: true,
   },
   output: 'export',
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
